4acfe3b668bfafae8e5eba349036471156680169
erikqb3 <erikqb3@gmail.com>
2022-08-20 13:23:20 -0600

    Trying to make PDFs downloadable from site

--- js/establishHTML.js
+++ js/establishHTML.js
@@ -34,8 +34,7 @@ export const establishHTML = {
     }
     //STEP3
     filterSettings_holder.appendChild(filterSettings_list);
-    header = this.appendChildren(header, name,titles,hamBtn,filterSettings_void,filterSettings_holder
-    );
+    header = this.appendChildren(header, name,titles,hamBtn,filterSettings_void,filterSettings_holder);
     document.querySelector('body').appendChild(header);
   },
   hero: function (
@@ -292,6 +291,46 @@ export const establishHTML = {
 };
 
 const helperFunctions = {
+  appendChildren: function (parent, ...elementChildren) {
+    for (let i in elementChildren) {
+      parent.appendChild(elementChildren[i]);
+    }
+    return parent;
+  },
+  cookieFunctions : {
+    checkCookie: function(cName) {
+      let string = getCookie(cName);
+      if (string != "") {
+      //  alert("Welcome again " + username);
+      } else {
+        // username = prompt("Please enter your name:", "");
+        if (string != "" && string != null) {
+          // setCookie("username", username, 365);
+        }
+      }
+    },
+    getCookie: function(cName) {
+      let name = cName + "=";
+      let decodedCookie = decodeURIComponent(document.cookie);
+      let cArray = decodedCookie.split(';');
+      for(let i = 0; i <cArray.length; i++) {
+        let c = cArray[i];
+        while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    },
+    setCookie: function (cName, cValue, exDays,cType) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exDays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cName + "=" + cValue + ";" + expires + ";path=/";
+},
+},
   generateElement: function (
     paramElement,
     paramId = '',
@@ -323,6 +362,50 @@ const helperFunctions = {
       element.innerHTML = paramText;
     }
     return element;
+  },
+  lazyLoading: function(
+    imagesToLoad = document.querySelectorAll('img[data-src]'), //images elements with the attribute "data-src"; similar to css #data-src or .data-src
+    loadImages = (img) => {
+      console.log("Img Loaded")
+      img.setAttribute('src', img.getAttribute('data-src'));
+      img.onload = () => {
+        img.removeAttribute('data-src');
+      }
+    },
+    imgOptions = {
+      threshold: 0,
+      rootMargin: "0px 0px -50px 0px" //make bottom positive so images load before entering screen;
+    },
+  ){
+    //imagesToLoad - 
+    //loadImages - 
+    //imgOptions - 
+    //Step1 - 
+    if ('IntersectionObserver' in window) {
+      const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+          if(item.isIntersecting) {
+            loadImages(item.target);
+            observer.unobserve(item.target);
+          }
+        });
+      }, imgOptions);
+      imagesToLoad.forEach((img) => {
+        observer.observe(img);
+      });
+    } else {
+      imagesToLoad.forEach((img)=> {
+        loadImages(img);
+      });
+    }
+  },
+  nestChildren: function(parent, ...elementChildren){
+    console.log(elementChildren)
+    parent.appendChild(elementChildren[0]);
+    for (let i=1; i<elementChildren.length; i++){
+        elementChildren[i-1].appendChild(elementChildren[i]);
+    }
+    return parent;
   },
   specialElements: function (element,extraAttribute = []) { //CLONE?
     switch (element.classList.value) {
